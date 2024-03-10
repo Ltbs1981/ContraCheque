3dad16727ff88beaa09be3f3b94072575c6b2dbc
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-03-10 03:40:01 -0300

    Fase 2, inserindo métodos.

--- Program.cs
+++ Program.cs
@@ -12,21 +12,33 @@ namespace Funcionario
             Console.WriteLine($"Informe o salário bruto de {nome}");
             double salarioBruto = double.Parse(Console.ReadLine());
             Console.WriteLine("Informe a porcentagem a ser descontada do salário para o IRRF");
-            double porcentagemDesconto = double.Parse(Console.ReadLine())/100;
+            double porcentagemDesconto = double.Parse(Console.ReadLine()) / 100;
 
-            Double desconto = salarioBruto *porcentagemDesconto;
+            double desconto = CalcularSalarioLiquido(salarioBruto, porcentagemDesconto);
             double salarioLiquido = salarioBruto - desconto;
 
-            Console.WriteLine($"Funcionário: {nome}. \n Desconto em folha: {desconto}. \n Total líquido: {salarioLiquido}");
+            Console.WriteLine($"Funcionário: {nome}. \n Desconto em folha: {desconto:f2}. \n Total líquido: {salarioLiquido:f2}");
 
             Console.WriteLine($"\n Informe quantos %de aumento {nome}, terá sobre seu salário bruto ");
             double porcentagemAumento = double.Parse(Console.ReadLine()) / 100;
 
-            Double aumento = salarioBruto * porcentagemAumento;
-            salarioBruto = salarioBruto + aumento;
+            salarioBruto = AumentarSalario(salarioBruto, porcentagemAumento);
 
-            Console.WriteLine($"Funcionário: {nome}. \n Bônus: {aumento}. \n Salário Bruto + aumento: {salarioBruto}");
+            Console.WriteLine($"Funcionário: {nome}. \n Bônus: {porcentagemAumento * 100}%. \n Salário Bruto + aumento: {salarioBruto}");
+        }
 
+        public static double CalcularSalarioLiquido(double salarioBruto, double porcentagemDesconto)
+        {
+            double desconto = salarioBruto * porcentagemDesconto;
+            return desconto;
+        }
+
+        public static double AumentarSalario(double salarioBruto, double porcentagemAumento)
+        {
+            double aumento = salarioBruto * porcentagemAumento;
+            salarioBruto += aumento;
+
+            return salarioBruto;
         }
     }
 }
